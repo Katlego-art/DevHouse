64626a4612c48ced06456e192f7879190942a389
Kat <142260481+Katlego-art@users.noreply.github.com>
2025-02-13 14:26:23 +0200

    Added some styles, html in index file(ejs) and connect header and footer

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-
-
-
 app.get("/",(req,res)=>{
   res.render("index.ejs");
 });
+
+
+
 
 app.listen(port, ()=>{
   console.log(`The app is running on port ${port}`);
