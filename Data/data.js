6320801658dac76d2e90780579f9c49c26162966
Kat <142260481+Katlego-art@users.noreply.github.com>
2025-02-17 16:42:16 +0200

    Added users and blogs

---
+++ Data/data.js
@@ -0,0 +1,16 @@
+const blog = [
+  {
+    ID: "ageidlewyGRU3274HD5656H",
+    Blog: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque faucibus massa nec nisl scelerisque, ac viverra odio aliquam. Duis luctus libero id purus lobortis ultricies. Mauris interdum, metus nec vehicula tempus, leo elit accumsan risus, et dignissim ex turpis sed augue. Nullam fermentum nisi non tortor ultricies accumsan. Aenean eget erat eu odio suscipit auctor. Phasellus porta metus in mi interdum, in fringilla eros accumsan. Aenean rutrum ex nec sollicitudin consequat. Nunc quis quam quis velit consectetur fringilla ac eu risus.",
+  },
+
+  {
+    ID: "ageidlewyGRUrtrtwt5r3274HDH",
+    Blog: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque faucibus massa nec nisl scelerisque, ac viverra odio aliquam. Duis luctus libero id purus lobortis ultricies. Mauris interdum, metus nec vehicula tempus, leo elit accumsan risus, et dignissim ex turpis sed augue. Nullam fermentum nisi non tortor ultricies accumsan. Aenean eget erat eu odio suscipit auctor. Phasellus porta metus in mi interdum, in fringilla eros accumsan. Aenean rutrum ex nec sollicitudin consequat. Nunc quis quam quis velit consectetur fringilla ac eu risus.",
+  },
+  {
+    ID: "ageidlewyGRU3w4w556556274HDH",
+    Blog: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque faucibus massa nec nisl scelerisque, ac viverra odio aliquam. Duis luctus libero id purus lobortis ultricies. Mauris interdum, metus nec vehicula tempus, leo elit accumsan risus, et dignissim ex turpis sed augue. Nullam fermentum nisi non tortor ultricies accumsan. Aenean eget erat eu odio suscipit auctor. Phasellus porta metus in mi interdum, in fringilla eros accumsan. Aenean rutrum ex nec sollicitudin consequat. Nunc quis quam quis velit consectetur fringilla ac eu risus.",
+  },
+];
+
---
+++ Data/user.js
@@ -0,0 +1,23 @@
+const users = [
+  {
+    username: "kat@gmail.com",
+    useremail: "kat@gmail.com",
+    Fname: "Kat Africa",
+    password: "naw@sdg41H",
+  },
+
+  {
+    username: "kat@gmail.com",
+    useremail: "kat@gmail.com",
+    Fname: "Kat Africa",
+    password: "naw@sdg41H",
+  },
+
+  {
+    username: "kat@gmail.com",
+    useremail: "kat@gmail.com",
+    Fname: "Kat Africa",
+    password: "naw@sdg41H",
+  },
+];
+
